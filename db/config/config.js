9f8e179a5dc73dc94d8dbeaa9834edf1cc046c66
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2023-12-07 20:09:19 -0300

    criacao do env com o postgres do render

--- db/config/config.js
+++ db/config/config.js
@@ -1,8 +1,12 @@
+import 'dotenv/config'
+
+//console.log(process.env.HOST);
+
 export const DB_CONFIG = {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    pass: '1234',
-    db: 'dc', // dripstore
-    dialect: 'postgres',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    pass: process.env.PASS,
+    db: process.env.DB, // dripstore
+    dialect: process.env.DIALECT,
 }
